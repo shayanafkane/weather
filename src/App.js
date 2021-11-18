456e197dcb9e3bb0a28860c2566a80d470e33b36
Shayan <87204203+shayanafkane@users.noreply.github.com>
2021-11-18 14:50:35 +0330

    Call-test-london

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Weather from "./app-component/weather.component";
+import 'weather-icons/css/weather-icons.css'
+import React, { Component } from "react";
 
 
-function App() {
-  return (
-    <div className="App">
+// api call api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+const API_key = "08d049aa2e64e568138bde85fdcd929e";
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_key}`);
+    const response = await api_call.json();
+
+    console.log(response)
+  }
+  render() {
+    return (
+      <div className="App">
         <Weather />
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/app-component/weather.component.jsx
+++ src/app-component/weather.component.jsx
@@ -3,8 +3,39 @@ import React from 'react';
 const Weather = () => {
     return(
         <div className="container">
-            <h1>Weather App</h1>
+            <div className="cards text-center">
+                <h1>London</h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny display-1">
+
+                    </i>
+                </h5>
+                <h1 className="py-2">
+                    25&deg;
+                </h1>
+
+
+                {/** show max and min */}
+                {minmaxTemp(24,19)}
+
+                <h4 className="py-3">
+                    Slow rain
+                </h4>
+            </div>
         </div>
     )
+}
+
+function minmaxTemp(min , max){
+    return(
+        <h3>
+            <span className="px-4">
+                {min}&deg;
+            </span>
+            <span className="px-4">
+                {max}&deg;
+            </span>
+        </h3>
+    );
 }
 export default Weather;
